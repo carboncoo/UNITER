49a49bbdef5b954e30c7a32481060396fb9b72ae
Maxy <thumaxy@gmail.com>
2021-06-18 03:39:10 +0000

    few-shot for ve

---
+++ data/fs_data.py
@@ -0,0 +1,64 @@
+import lmdb
+import json
+import random
+import os
+from os.path import exists
+# from toolz.sandbox import unzip
+
+
+db_dir = "/data/share/UNITER/ve/txt_db/ve_train.db"
+output_db_dir = "/data/share/UNITER/ve_fewshot/txt_db/ve_train.db"
+s = 1
+num_of_sample = 32
+random.seed(s)
+
+def main():
+    # import ipdb; ipdb.set_trace()
+    output_dir = output_db_dir + '/seed_%d'%(s)
+    if not exists(output_dir):
+        os.makedirs(output_dir)
+    else:
+        raise ValueError('Found existing DB. Please explicitly remove '
+                         'for re-processing')
+
+    # read from db
+    env_in = lmdb.open(db_dir)
+    txn_in = env_in.begin()
+    db = {}
+    for key, value in txn_in.cursor():
+        db[key] = value
+    print('db length:', len(db))
+    env_in.close()
+
+    id2len = json.load(open(db_dir + '/id2len.json'))
+    img2txts = json.load(open(db_dir + '/img2txts.json'))
+    txt2img = json.load(open(db_dir + '/txt2img.json'))
+    print(len(id2len))
+    print(len(img2txts))
+    print(len(txt2img))
+
+    # random sample
+    out_img2txts = dict(random.sample(img2txts.items(), num_of_sample))
+    txts = [txt for sublist in out_img2txts.values() for txt in sublist]
+    out_id2len = {k: id2len[k] for k in txts}
+    out_txt2img = {k: txt2img[k] for k in txts}
+    out_db = {k.encode('utf-8'): db[k.encode('utf-8')] for k in txts}
+    # print(out_id2len)
+    # print(out_txt2img)
+    # print(out_db)
+    
+    # write to db
+    env_out = lmdb.open(output_dir)
+    txn_out = env_out.begin(write=True)
+    for k, v in out_db.items():
+        txn_out.put(k, v)
+    txn_out.commit()
+    env_out.close()
+
+    json.dump(out_id2len, open(output_dir + '/id2len.json', 'w'))
+    json.dump(out_img2txts, open(output_dir + '/img2txts.json', 'w'))
+    json.dump(out_txt2img, open(output_dir + '/txt2img.json', 'w'))
+
+
+if __name__ == "__main__":
+	main()
---
+++ train.sh
@@ -0,0 +1,31 @@
+ROOT=/data/private/mxy
+CODE=$ROOT/code/cc-UNITER
+CKPT=$ROOT/exp/UNITER/pretrained
+EXP=$ROOT/exp/UNITER/finetune
+IMG_DB=/data/share/UNITER/img_db
+TXT_DB=/data/share/UNITER/txt_db
+export CUDA_VISIBLE_DEVICES=0,1
+
+# train nlvr2
+# CUDA_VISIBLE_DEVICES=5 python train_nlvr2.py --config config/train-softprompt-nlvr2-base-1gpu.json
+
+# train vcr
+VCR_EXP=$EXP/vcr/default
+# nohup horovodrun -np 4 python train_vcr.py --config config/train-vcr-base-4gpu.json \
+#     --output_dir $VCR_EXP > vcr.out 2>&1 &
+
+# train ve
+VE_EXP=$EXP/ve_fewshot/seed_1
+horovodrun -np 2 python train_ve.py --config config/train-ve-fs-base-2gpu.json \
+    --output_dir $VE_EXP &
+
+# zero-shot itr
+NGPU=6
+ZS_ITM_RESULT=$EXP/itr/zs_result
+# horovodrun -np $NGPU python inf_itm.py \
+#     --txt_db $TXT_DB/itm_flickr30k_test.db --img_db $IMG_DB/flickr30k \
+#     --checkpoint $CKPT/uniter-base.pt --model_config $CODE/config/uniter-base.json \
+#     --output_dir $ZS_ITM_RESULT --fp16 --pin_mem
+
+# itr normal finetune
+# nohup horovodrun -np 4 python train_itm.py --config config/train-itm-flickr-base-8gpu.json > itr_flickr_normal.out 2>&1 &
