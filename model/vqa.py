59728f88a4eb6970aceb77db78be975645a9a79b
carboncoo <chenchi19@mails.tsinghua.edu.cn>
2021-06-28 09:09:04 +0000

    prompt decoder

--- model/vqa.py
+++ model/vqa.py
@@ -50,3 +50,59 @@ class UniterForVisualQuestionAnswering(UniterPreTrainedModel):
             return vqa_loss
         else:
             return answer_scores
+        
+        
+class UniterSoftPromptForVisualQuestionAnswering(UniterPreTrainedModel):
+    """ Finetune UNITER with soft prompts for VQA
+    """
+    def __init__(self, config, img_dim, *inputs, **kwargs):
+        super().__init__(config)
+        self.uniter_softprompt = UniterSoftPromptModel(config, img_dim, *inputs, **kwargs)
+        self.apply(self.init_weights)
+        self.uniter_softprompt.set_hard_prompt('[MASK] It is just .')
+        self.label_mapping = kwargs.get('label_mapping', [0])
+
+    @classmethod
+    def from_pretrained(cls, config_file, state_dict, *inputs, **kwargs):
+        config = UniterConfig.from_json_file(config_file)
+        model = cls(config, *inputs, **kwargs)
+        model.uniter_softprompt = UniterSoftPromptModel.from_pretrained(config_file, state_dict, *inputs, **kwargs)
+        model.uniter_softprompt.set_hard_prompt('[MASK] It is just .')
+        
+        # TODO: 
+        #   use vqa label_mapping to reset "model.uniter_softprompt.cls.predictions.decoder"
+        #   for class described with multiple words, use their mean pooling
+        #   make sure to also set bias accordingly
+        
+        class_weights = model.uniter_softprompt.uniter.embeddings.word_embeddings.weight[kwargs.get('label_mapping', [0])].clone()
+        model.uniter_softprompt.cls.predictions.decoder = nn.Linear(
+                                    class_weights.size(1),
+                                    class_weights.size(0),
+                                    bias=False)
+        model.uniter_softprompt.cls.predictions.decoder.weight.data = class_weights
+        model.uniter_softprompt.cls.predictions.bias = nn.Parameter(model.uniter_softprompt.cls.predictions.bias.data[kwargs.get('label_mapping', [0])].clone())
+        return model
+
+    def forward(self, batch, compute_loss=True):
+        batch = defaultdict(lambda: None, batch)
+        input_ids = batch['input_ids']
+        position_ids = batch['position_ids']
+        img_feat = batch['img_feat']
+        img_pos_feat = batch['img_pos_feat']
+        attn_masks = batch['attn_masks']
+        gather_index = batch['gather_index']
+        sequence_output = self.uniter_softprompt(input_ids, position_ids,
+                                      img_feat, img_pos_feat,
+                                      attn_masks, gather_index,
+                                      output_all_encoded_layers=False)
+        
+        predicted_output = sequence_output[:, self.uniter_softprompt.prediction_pos, :]
+        answer_scores = self.uniter_softprompt.cls(predicted_output)
+
+        if compute_loss:
+            targets = batch['targets']
+            vqa_loss = F.binary_cross_entropy_with_logits(
+                answer_scores, targets, reduction='none')
+            return vqa_loss
+        else:
+            return answer_scores
